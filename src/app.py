ee66e92dcf06db627d235d5ecd353d1557f3d1e8
NewGyu <newgyu@kinoboku.net>
2016-06-02 23:19:01 +0900

    何回もtf.Session()関数を読んだのがいかんかった？

--- src/app.py
+++ src/app.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
+sess = tf.Session()
+
 @app.route("/healthcheck")
 def healthcheck():
   return "OK"
@@ -25,15 +27,14 @@ def classify():
 def classify_sample(sampleId):
   a = tf.constant(10)
   b = tf.constant(32)
-  with tf.Session() as sess:
-    r = sess.run(a + b)
-    result = {
-      "host_rate": 0.4,
-      "villain_rate": 0.32,
-      "jhonnys_rate": 0.1,
-      "yoshimoto_rate": r.item(0)  #numpyは直でJSON Serializeできない・・・
-    }
-    return jsonify(result)
+  r = sess.run(a + b)
+  result = {
+    "host_rate": 0.4,
+    "villain_rate": 0.32,
+    "jhonnys_rate": 0.1,
+    "yoshimoto_rate": r.item(0)  #numpyは直でJSON Serializeできない・・・
+  }
+  return jsonify(result)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
